7ec49cb1967b6dc99ffe0de807f3ba26b5e3a514
Le Dinh Luong <ledinhluong.std@gmail.com>
2021-04-02 22:29:00 +0700

    12

--- index.js
+++ index.js
@@ -12,12 +12,52 @@ app.set('view engine', 'ejs');
 // app.use(express.static(__dirname + '/public'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
-	// ejs render automatically looks in the views folder
-	res.render('index');
+  // ejs render automatically looks in the views folder
+  res.render('index');
 });
 
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+
+
+/**
+ *  SOCKET.IO
+ */
+
+let nextVisitorNumber = 1;
+const onlineClients = new Set();
+var io = require("socket.io")(app);
+io.on("connection", onNewWebsocketConnection);
+
+function onNewWebsocketConnection(socket) {
+  console.info(`Socket ${socket.id} has connected.`);
+  onlineClients.add(socket.id);
+
+  socket.on("disconnect", () => {
+    onlineClients.delete(socket.id);
+    console.info(`Socket ${socket.id} has disconnected.`);
+  });
+
+  // echoes on the terminal every "hello" message this socket sends
+  socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: "${helloMsg}"`));
+
+  // will send a message only to this socket (different than using `io.emit()`, which would broadcast it)
+  socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
+
+  socket.on("send sms", data => {
+    console.log(data);
+    io.emit("client send sms", data);
+  });
+}
+
+let secondsSinceServerStarted = 0;
+setInterval(() => {
+  secondsSinceServerStarted++;
+  io.emit("seconds", secondsSinceServerStarted);
+  io.emit("online", onlineClients.size);
+}, 1000);
+
+
+app.listen(port, function () {
+  console.log('Our app is running on http://localhost:' + port);
 });
